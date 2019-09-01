679eba4a5c706bd2046c549012082c9f2887c953
xenophorus <xenophorus@gmail.com>
2019-09-01 19:01:29 +1000

    Начал в нем занятия по курсу Udemy

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,20 @@
-print("Hello world")
-input()
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+    return the_mean
+
+
+a = [5, 32, 5, 23, 76, 3, 12, 7, 4, 5]
+stud_grades = {'Steve': 5.5, 'Lynn': 6.2, 'Ingrid': 7.1}
+
+print(mean(a))
+print(mean(stud_grades))
+
+user_input = input("Enter your name: ")
+message1 = "Hello, %s!" % user_input
+message2 = "Hello, {}!".format(user_input)
+
+print(message1)
+print(message2)
