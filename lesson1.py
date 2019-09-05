61026156123d8ed60c5e10e0a67f9f94652d2ba8
xenophorus <xenophorus@gmail.com>
2019-09-05 11:44:24 +1000

    Продолжаем

--- lesson1.py
+++ lesson1.py
@@ -12,9 +12,20 @@ stud_grades = {'Steve': 5.5, 'Lynn': 6.2, 'Ingrid': 7.1}
 print(mean(a))
 print(mean(stud_grades))
 
-user_input = input("Enter your name: ")
-message1 = "Hello, %s!" % user_input
-message2 = "Hello, {}!".format(user_input)
+# user_input = input("Enter your name: ")
+# message1 = "Hello, %s!" % user_input
+# message2 = "Hello, {}!".format(user_input)
+#
+# print(message1)
+# print(message2)
 
-print(message1)
-print(message2)
+def greatName(sName, sSurname):
+    tName = sName[0].upper() + sName[1:].lower()
+    tSurname = sSurname[0].upper() + sSurname[1:].lower()
+    return 'Hi %s %s' % (tName, tSurname)
+
+print(greatName('ann', 'johnson'))
+
+
+
+
