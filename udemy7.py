9b0aba1d6830ff021048dc857e7a2477294fdfc0
xenophorus <xenophorus@gmail.com>
2019-09-23 20:10:32 +1000

    Udemy lessons

--- udemy7.py
+++ udemy7.py
@@ -1,3 +1,35 @@
-myfile = open('fruits.txt')
+# file = open('fruits.txt')
+# content = file.read()
+# file.close()
+#
+# print(content)
+
+# def lettercount(letter, file):
+#     myfile = open(file)
+#     cont = myfile.read()
+#     return cont.count(letter)
+#
+# def lettercount(letter, file):
+#     with open(file) as myfile:
+#         cont = myfile.read()
+#         return cont.count(letter)
+#
+# print(lettercount('c', 'textfiles/bear.txt'))
+
+#
+# with open('textfiles/vegetables.txt', 'w') as myfile:
+#     myfile.write('Tomato\nPotato')
+
+# with open('textfiles/bear1.txt') as myfile:
+#      tbear = myfile.read()
+#
+# with open('textfiles/bear2.txt', 'a+') as bears:
+#     bears.write(tbear)
+
+with open('textfiles/data.txt', 'r') as f:
+    cont = f.read()
 
 
+with open('textfiles/data.txt', 'a+') as f:
+    f.write(cont)
+    f.write(cont)
