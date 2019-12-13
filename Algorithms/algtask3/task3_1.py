990452cb24ea4a2483146d93abcdac441c4ab9d5
xenophorus <xenophorus@gmail.com>
2019-12-13 20:05:59 +1000

    completed

--- Algorithms/algtask3/task3_1.py
+++ Algorithms/algtask3/task3_1.py
@@ -160,12 +160,94 @@
 # В одномерном массиве целых чисел определить два наименьших элемента.
 # Они могут быть как равны между собой (оба являться минимальными), так и различаться.
 
+# import random
+#
+#
+# def min_num(lst):
+#     lst_min = 100
+#     for i in lst:
+#         if lst_min >= i:
+#             lst_min = i
+#     return lst_min
+#
+#
+# def begin():
+#     arr_length = 10
+#     arr_min = 1
+#     arr_max = 100
+#     lst = [random.randint(arr_min, arr_max) for _ in range(arr_length)]
+#     print(lst)
+#     a = min_num(lst)
+#     lst.remove(a)
+#     b = min_num(lst)
+#     print(a, b)
+#
+#
+# begin()
 
 
 
 # Матрица 5x4 заполняется вводом с клавиатуры, кроме последних элементов строк.
 # Программа должна вычислять сумму введенных элементов каждой строки и записывать
 # ее в последнюю ячейку строки. В конце следует вывести полученную матрицу.
+# import sys
+#
+#
+# def m_input():
+#     s = input('Введите 3 числа, разделенных пробелами: ')
+#     s.strip()
+#     a = s.strip().split(' ')
+#     sum_ = 0
+#     for i in a.copy():
+#         if i.isdigit() == False:
+#             print('Некорректный ввод!')
+#             sys.exit()
+#         elif len(a) != 3:
+#             print('Неверное число цифр!')
+#             sys.exit()
+#         else:
+#             a.append(int(a.pop(0)))
+#             sum_ += int(i)
+#     a.append(sum_)
+#     return a
+#
+#
+# def begin():
+#     print('Заполним матрицу 5x4.')
+#     matrix = []
+#     for i in range(5):
+#         matrix.append(m_input())
+#     print(*matrix, sep = '\n')
+#
+#
+# begin()
 
 
 # Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+import random
+
+
+
+def begin():
+    matrix = []
+    for i in range(5):
+        matrix.append([random.randint(1, 100) for i in range(5)])
+    print(*matrix, sep='\n')
+    mins = []
+    for i in matrix:
+        a = 100
+        for j in i:
+            if a >= j:
+                a = j
+        mins.append(a)
+    print(mins)
+    a = 0
+    for i in mins:
+        if a <= i:
+            a = i
+    print(f'Максимальное число из минимальных {a}')
+
+
+
+
+begin()
