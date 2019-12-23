b76838e29276be2057a1514680c83a055bb42dc4
xenophorus <xenophorus@gmail.com>
2019-12-23 21:12:07 +1000

    1

--- Algorithms/less4.py
+++ Algorithms/less4.py
@@ -1,4 +1,15 @@
-import timeit
-print(timeit.timeit("x = 2 + 2"))
-print(timeit.timeit("x = sum(range(1000))"))
-print(sum(range(10)))
+# import timeit
+# print(timeit.timeit("x = 2 + 2"))
+# print(timeit.timeit("x = sum(range(1000))"))
+# print(sum(range(10)))
+
+import collections
+
+counter = collections.Counter()
+for word in 'crabalocker':
+    counter[word] += 1
+print(counter)
+print(counter['counter'])
+print(counter['collections'])
+
+print(list(counter.elements()))
