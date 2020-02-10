febad2e1f8c6694df7d59d09a8a88d67222393f0
xenophorus <xenophorus@gmail.com>
2020-02-10 20:14:27 +1000

    cont

--- Udemy_Lessons/sect13_eng_dict.py
+++ Udemy_Lessons/sect13_eng_dict.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data/data.json'))
 
@@ -6,6 +7,8 @@ data = json.load(open('data/data.json'))
 def translate(word):
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return f'Did you mean {get_close_matches(word, data.keys())[0]} instead?'
     else:
         return "The word does not exist. Please check it"
 
